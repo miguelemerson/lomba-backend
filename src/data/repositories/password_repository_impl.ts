dbc8acf0f3d2f92da86b6cb8463032b22cccf928
miguelemerson <mperedo@gmail.com>
2023-01-05 18:23:37 -0300

    cambios ultimos

--- src/data/repositories/password_repository_impl.ts
+++ src/data/repositories/password_repository_impl.ts
@@ -1,12 +1,12 @@
 import { MongoError } from 'mongodb';
-import { ModelContainer } from '../../core/model_container';
-import { PasswordRepository } from '../../domain/repositories/password_repository';
-import { Auth } from '../../domain/entities/auth';
-import { PasswordModel } from '../models/password_model';
-import { DatabaseFailure, NetworkFailure, GenericFailure, Failure } from '../../core/errors/failures';
 import { Either } from '../../core/either';
+import { DatabaseFailure, Failure, GenericFailure, NetworkFailure } from '../../core/errors/failures';
+import { ModelContainer } from '../../core/model_container';
+import { HashPassword } from '../../core/password_hash';
+import { Auth } from '../../domain/entities/auth';
+import { PasswordRepository } from '../../domain/repositories/password_repository';
 import { PasswordDataSource } from '../datasources/password_data_source';
-import { Token } from '../../domain/entities/token';
+import { PasswordModel } from '../models/password_model';
 
 export class PasswordRepositoryImpl implements PasswordRepository {
 	dataSource: PasswordDataSource;
@@ -16,7 +16,13 @@ export class PasswordRepositoryImpl implements PasswordRepository {
 
 	async addPassword(userId:string, auth:Auth): Promise<Either<Failure,ModelContainer<PasswordModel>>>{
 		try{
-			const pass: PasswordModel = new PasswordModel(userId, '','',true,false);
+			//debe especificar password caso contrario retorna left
+			if(!auth.password)
+				return Either.left(new GenericFailure('Need password'));
+
+			const passHashed = HashPassword.createHash(auth.password);
+
+			const pass: PasswordModel = new PasswordModel(userId, passHashed.hash,passHashed.salt,true,false);
 			const result = await this.dataSource.add(pass);
 			return Either.right(result);
 		}
@@ -33,43 +39,15 @@ export class PasswordRepositoryImpl implements PasswordRepository {
 	}
 	async updatePassword(userId:string, auth:Auth): Promise<Either<Failure,ModelContainer<PasswordModel>>>{
 		try{
-			const result = await this.dataSource.update(userId, auth);
-			return Either.right(result);
-		}
-		catch(error)
-		{
-			if(error instanceof MongoError)
-			{
-				return Either.left(new DatabaseFailure(error.name, error.message, error.code, error));
-			} else if(error instanceof Error)
-				return Either.left(new NetworkFailure(error.name, error.message, undefined, error));
-			else return Either.left(new GenericFailure('undetermined', error));
-			
-		}
-	}
+			//debe especificar password caso contrario retorna left
+			if(!auth.password)
+				return Either.left(new GenericFailure('Need password'));
 
-	async getPassword(userId: string): Promise<Either<Failure,ModelContainer<PasswordModel>>> {
-		try
-		{
-			const result = await this.dataSource.getOne(userId);
-			return Either.right(result);
-		}
-		catch(error)
-		{
-			if(error instanceof MongoError)
-			{
-				return Either.left(new DatabaseFailure(error.name, error.message, error.code, error));
-			} else if(error instanceof Error)
-				return Either.left(new NetworkFailure(error.name, error.message, undefined, error));
-			else return Either.left(new GenericFailure('undetermined', error));
-			
-		}
-	}
+			const passHashed = HashPassword.createHash(auth.password);
 
-	async getAuth(auth:Auth): Promise<Either<Failure,ModelContainer<PasswordModel>>> {
-		try
-		{
-			const result = await this.dataSource.getOne('');
+			const changes = {hash:passHashed.hash, salt:passHashed.salt, istemp:false};
+
+			const result = await this.dataSource.update(userId, changes);
 			return Either.right(result);
 		}
 		catch(error)
