73b4b38478fb3a183d8b2c1f1030578b22e6de5a
miguelemerson <mperedo@gmail.com>
2023-01-19 17:49:45 -0300

    cambio de exist user a post

--- src/data/repositories/user_repository_impl.ts
+++ src/data/repositories/user_repository_impl.ts
@@ -118,7 +118,6 @@ export class UserRepositoryImpl implements UserRepository {
 		{
 			
 			const result = await this.dataSource.getOne({$or:[ {'username':username}, {'email':email}]});
-			console.log(result);
 			
 			return Either.right(result);
 		}
