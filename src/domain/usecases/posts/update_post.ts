f830a2fbb7b88237cbf927f85ee1c08b575099b1
miguelemerson <mperedo@gmail.com>
2023-05-08 16:22:50 -0400

    se editan los post

--- src/domain/usecases/posts/update_post.ts
+++ src/domain/usecases/posts/update_post.ts
@@ -4,9 +4,11 @@ import { ModelContainer } from '../../../core/model_container';
 import { Post } from '../../entities/workflow/post';
 import { PostRepository } from '../../repositories/post_repository';
 import { TextContent } from '../../entities/workflow/textcontent';
+import { ImageContent } from '../../entities/workflow/imagecontent';
+import { VideoContent } from '../../entities/workflow/videocontent';
 
 export interface UpdatePostUseCase {
-    execute(postId: string, userId: string, title: string, textContent: TextContent): Promise<Either<Failure,ModelContainer<Post>>>;
+    execute(postId: string, userId: string, title: string, textContent: TextContent | undefined, imageContent: ImageContent | undefined, videoContent: VideoContent | undefined): Promise<Either<Failure,ModelContainer<Post>>>;
 }
 
 export class UpdatePost implements UpdatePostUseCase {
@@ -15,7 +17,7 @@ export class UpdatePost implements UpdatePostUseCase {
 		this.repository = repository;
 	}
 
-	async execute(postId: string, userId: string, title: string, textContent: TextContent): Promise<Either<Failure,ModelContainer<Post>>> {
-		return await this.repository.updatePost(postId, userId, title, textContent);
+	async execute(postId: string, userId: string, title: string, textContent: TextContent | undefined, imageContent: ImageContent | undefined, videoContent: VideoContent | undefined): Promise<Either<Failure,ModelContainer<Post>>> {
+		return await this.repository.updatePost(postId, userId, title, textContent, imageContent, videoContent);
 	}
 }
